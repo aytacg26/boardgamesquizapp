b5d2c6ce37e69ffb054a5b9921fc51349bbd8a2c
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-02 17:34:16 +0300

    redirect URI option added to function

--- js/utils/clearLocalStoreage.js
+++ js/utils/clearLocalStoreage.js
@@ -4,10 +4,10 @@
  * It clears the localStorage and redirects the player to the start page / main page of the app.
  * @param {Boolean} removeName - if we would like to also remove the name of the user from localStore, this will be true (default = false)
  */
-export const clearLocalStorage = (removeName = false) => {
+export const clearLocalStorage = (removeName = false, redirectTo = '/') => {
   localStorage.removeItem('NumOfQuestions');
   localStorage.removeItem('points');
   localStorage.removeItem('currentQuestion');
   removeName ? localStorage.removeItem('player') : '';
-  window.location.replace('/');
+  window.location.replace(redirectTo);
 };
