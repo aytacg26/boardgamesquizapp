dd166668dfcab201af1e61daf2a457c3c1d644ac
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-02 21:10:54 +0300

    rootResultsObject created for lazy loading

---
+++ js/statisticsWindow/resultFunctions.js
@@ -0,0 +1,13 @@
+import { calculateTotalPoints } from '../utils/calculateTotalPoints.js';
+import { pointStatCalculator } from '../utils/pointStatCalculator.js';
+import { statDataSelector } from '../utils/statDataSelector.js';
+import { StatWin } from './statWin.js';
+
+const resultFunctions = {
+  calculateTotalPoints,
+  pointStatCalculator,
+  statDataSelector,
+  StatWin,
+};
+
+export default resultFunctions;
