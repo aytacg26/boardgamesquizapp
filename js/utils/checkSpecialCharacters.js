8d6f611cceaffe862e391f2cf7fc0c02f45e8e3e
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-02 17:32:52 +0300

    special character checking function improved

--- js/utils/checkSpecialCharacters.js
+++ js/utils/checkSpecialCharacters.js
@@ -1,6 +1,50 @@
 export const checkSpecialCharacters = (value) => {
-  const specialChars = /[^A-Za-z 0-9]/g;
-  const hasSpecialChars = specialChars.test(value);
+  const specialChars = /[!@#$%^&*()/+=~€£;_,.?":{}|<>]/g;
+  let hasSpecialChars = true;
 
-  return { hasSpecialChars };
+  if (typeof value === 'string') {
+    //can be string or number or true/false
+    hasSpecialChars = specialChars.test(value);
+
+    return { hasSpecialChars };
+  } else if (typeof value === 'object' && Array.isArray(value)) {
+    //can be an string array or number array or boolean array,
+    //in any case typeof each item from input will be string
+    value.forEach((item) => {
+      if (specialChars.test(item)) {
+        return { hasSpecialChars: true };
+      } else {
+        //each item won't pass the test of specialChars, which means they do not have any special char, otherwise
+        //loop will stop in if block
+        hasSpecialChars = false;
+      }
+    });
+
+    //This will always return false, we cannot apply this return process in loop
+    return { hasSpecialChars };
+  } else if (typeof value === 'object') {
+    //we may have an object which is not an array, we need to check each value of object key {key:value}
+    //From input element, this will come as string and will not pass the test in first if statement and will
+    //never reach this point. This part created for other possible use cases.
+    const keys = Object.keys();
+
+    if (keys) {
+      keys.forEach((key) => {
+        if (specialChars.test(value[key])) {
+          return { hasSpecialChars: true };
+        } else {
+          //each item won't pass the test of specialChars, which means they do not have any special char, otherwise
+          //loop will stop in if block
+          hasSpecialChars = false;
+        }
+      });
+
+      //This will always return false, we cannot apply this return process in loop
+      return { hasSpecialChars };
+    } else {
+      return { hasSpecialChars };
+    }
+  } else {
+    return { hasSpecialChars };
+  }
 };
